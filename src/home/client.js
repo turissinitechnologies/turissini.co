4a97c18c2b1803bf40454338391e50b462b0a036
Dave <davidairborne@gmail.com>
2016-03-04 09:18:54 -0700

    Adding springs and highlighting nav items as user scrolls

--- src/home/client.js
+++ src/home/client.js
@@ -2,13 +2,49 @@
 
 import './client/parallax';
 import './client/contactForm';
+import mSpring from './client/lib/mspring';
+import delegateEvents from 'delegate-events';
+
+function toggleClassName (el, className) {
+    if (el.classList.contains(className)) {
+        el.classList.remove(className);
+    } else {
+        el.classList.add(className);
+    }
+}
 
 
-import jquery from 'jquery';
+const siteHeader = document.getElementById('site-header');
+const siteNavBarCollapse = document.getElementById('site-navbar-collapse');
+const navbarButton = document.getElementById('site-navbar-button');
+navbarButton.addEventListener('click', function () {
+    toggleClassName(siteNavBarCollapse, 'in');
+    toggleClassName(navbarButton, 'active');
+    toggleClassName(siteHeader, 'navbar-open');
+});
 
-const siteNavBarCollapse = jquery('#site-navbar-collapse');
-const navbarButton = jquery('#site-navbar-button');
-navbarButton.click(function () {
-    siteNavBarCollapse.toggleClass('in');
-    navbarButton.toggleClass('active');
+var demoSpring = new mSpring({
+	onSpringStart: function() {
+
+	},
+	onSpringChange: function(massPos, springData) {
+        window.requestAnimationFrame(function () {
+            window.scrollTo(0, massPos);
+        });
+	},
+	onSpringRest: function() {
+        
+	}
+});
+
+demoSpring.setSpringParameters(5, 1, 0.6);
+
+delegateEvents.bind(siteHeader, '.navbar-nav a', 'click', function (e) {
+    e.preventDefault();
+    const target = e.target;
+    const href = target.getAttribute('href');
+    const el = document.querySelector(href);
+    const offsetTop = el.offsetTop;
+
+    demoSpring.start(undefined, window.scrollY, undefined, offsetTop);
 });
--- src/home/client/parallax.js
+++ src/home/client/parallax.js
@@ -4,12 +4,43 @@ import { createRotoscope } from 'rotoscope';
 
 const siteHeader = document.getElementById('site-header');
 const hero = document.getElementById('hero');
-
+const aboutNavLink = document.getElementById('nav-link-about');
+const aboutSection = document.getElementById('about');
 
 function parallax() {
     const scrollHeight = document.body.offsetHeight - window.innerHeight;
     const heroHeight = hero.offsetHeight;
     const headerHeight = siteHeader.offsetHeight;
+    const aboutOffsetHeight = aboutSection.offsetHeight;
+    const aboutOffsetTop = aboutSection.offsetTop;
+
+    function createNavbarHighlight (sectionId) {
+        const section = document.getElementById(sectionId);
+        const anchor = document.querySelector(`a[href="#${sectionId}"]`);
+
+        const clip = function (time) {
+            if (time > 0 && time < 1) {
+                return function () {
+                    anchor.classList.add('highlight');
+                }
+            }
+
+            return function () {
+                anchor.classList.remove('highlight');
+            }
+        }
+
+        const offsets = {
+            offset: section.offsetTop - headerHeight,
+            duration: section.offsetHeight
+        };
+
+        return {
+            clip,
+            offsets
+        };
+    }
+
 
     return createRotoscope(window).bounds({
         start: 0,
@@ -27,10 +58,28 @@ function parallax() {
             }
         }
 
+
+
+        const highlightAboutNavButton = createNavbarHighlight('about');
+        const highlightServicesNavButton = createNavbarHighlight('services');
+        const highlightContactNavButton = createNavbarHighlight('contact');
+
         return timeline.appendChild(animateSiteHeaderBackground, {
             offset: heroHeight / 2,
             duration: (heroHeight / 2) - headerHeight
-        });
+        })
+        .appendChild(
+            highlightAboutNavButton.clip,
+            highlightAboutNavButton.offsets
+        )
+        .appendChild(
+            highlightServicesNavButton.clip,
+            highlightServicesNavButton.offsets
+        )
+        .appendChild(
+            highlightContactNavButton.clip,
+            highlightContactNavButton.offsets
+        );
 
 
     })
